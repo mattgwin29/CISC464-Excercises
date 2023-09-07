fd648692ddd5b2f71d8d523384f7c2df027fadac
mattgwin29 <mattgwin@udel.edu>
2023-09-07 09:20:33 -0400

    Fix https for jquery link exercise 3

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 $( document ).ready(function() {
 console.log( "ready!" );
 $("#go").on("click", (event) => event.preventDefault($("#jumbotron").text($("#fname").val() + ' ' + $("#lname").val())));
-
-
 });
 
