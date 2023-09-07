344b4fe9d7c76b4081de1b6b0fa4b46e1394664e
mattgwin29 <mattgwin@udel.edu>
2023-09-07 09:03:44 -0400

    Add on click event for the submit button to add text to the site

---
+++ index.js
@@ -0,0 +1,7 @@
+$( document ).ready(function() {
+console.log( "ready!" );
+$("#go").on("click", (event) => event.preventDefault($("#jumbotron").text($("#fname").val() + ' ' + $("#lname").val())));
+
+
+});
+
